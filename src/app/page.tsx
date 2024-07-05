94bbecc301886467ccbc2b2e7bdb3dd3d90772bd
Abhirath Dubey <abhirathdubey2804@gmail.com>
2024-07-05 15:46:29 +0530

    index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,33 @@
-"use client";
-
-import * as z from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-
-const formSchema = z.object({
-  emailAddress: z.string().email(),
-  password: z.string().min(3),
-  passwordConfirm: z.string()
-}).refine((data) => {
-  return data.password === data.passwordConfirm
-}, {
-  message: "Passwords do not match",
-  path: ["passwordConfirm"]
-})
+import Link from "next/link"
 
 export default function Home() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      emailAddress: "",
-    },
-  });
-
-  const handleSubmit = () => {};
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <Navbar /> */}
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(handleSubmit)} className="max-w-md w-full flex flex-col gap-4">
-          <FormField
-            control={form.control}
-            name="emailAddress"
-            render={({ field }) => {
-              return (
-                <FormItem>
-                  <FormLabel>Email Address</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Email Address" type="email" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              );
-            }}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => {
-              return (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Password" type="password" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              );
-            }}
-          />
-          <FormField
-            control={form.control}
-            name="passwordConfirm"
-            render={({ field }) => {
-              return (
-                <FormItem>
-                  <FormLabel>Confirm Password</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Confirm Password" type="password" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              );
-            }}
-          />
-          <Button type="submit" className="w-full">
-            Submit
-          </Button>
-        </form>
-      </Form>
-    </main>
+  <div className="p-10">
+    Below are all the pages, click to visit: <br />
+    <Link
+      href="/auth/signin"
+      className="text-cyan-500 transition-colors hover:text-cyan-400"
+    >
+      Signin
+    </Link><br />
+    <Link
+      href="/auth/signup"
+      className="text-cyan-500 transition-colors hover:text-cyan-400"
+    >
+      Signup
+    </Link><br />
+    <Link
+      href="/campaigns"
+      className="text-cyan-500 transition-colors hover:text-cyan-400"
+    >
+      Campaigns
+    </Link> <br />
+    <Link
+      href="/agents"
+      className="text-cyan-500 transition-colors hover:text-cyan-400"
+    >
+      Agents
+    </Link>
+  </div>  
   );
 }
